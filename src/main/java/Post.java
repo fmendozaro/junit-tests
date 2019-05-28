8d6806b42e800e7d729500f8b5225cb490b5e2c7
Fer <fernandoza@gmail.com>
2019-05-27 21:30:38 -0500

    test: see save post fail

--- src/main/java/Post.java
+++ src/main/java/Post.java
@@ -8,6 +8,13 @@ public class Post {
     long userId;
 
     public Post(String title, String content, long userId) {
+        this.title = title;
+        this.content = content;
+        this.userId = userId;
         posts = new ArrayList<Post>();
+    }
+
+    public Post save(Post post) {
+        return null;
     }
 }
--- src/test/java/PostTest.java
+++ src/test/java/PostTest.java
@@ -1,12 +1,15 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class PostTest {
 
     @Test
     public void testAddPost(){
-        Post post = new Post("title", "content", 1L);
+        Post post = new Post("ps4", "for sale", 1L);
         assertNotNull(post);
+        assertEquals("ps4", post.title);
+        Post savedPost = post.save(post);
+        assertNotNull(savedPost);
     }
 }
